f68543fe60ac3401f4e9e7b25ca485ce983773e3
Tessa Alexander <tessa.alexander@bristol.ac.uk>
2020-02-19 12:55:41 +0000

    Lesson 0 - Explore basic HTTP response handling

--- lesson-0/main.go
+++ lesson-0/main.go
@@ -9,8 +9,10 @@ func main() {
 	http.HandleFunc("/", helloWorld)
 	log.Println("service started on :8080")
 	http.ListenAndServe(":8080", nil)
+	log.Println("Am I ever run??? Nope!")
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
+	log.Println("Log all the things")
 	w.Write([]byte("Hello, world!"))
 }
